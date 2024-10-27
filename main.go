b7f57b7f510135fd8fc6d514716cb88860624b8a
Ysh <3248433053@qq.com>
2024-10-27 11:28:39 +0800

    添加发送告警至企微功能，告警信息附带kibana查询链接

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
 	"github.com/olivere/elastic/v7"
 	"log"
+	"net/http"
 	"reflect"
 	"time"
 )
@@ -62,23 +65,58 @@ func main() {
 
 	// 用于计数和存储前三条消息
 	count := 0
-
+	var errorMessages []string
 	for _, item := range searchResult.Each(reflect.TypeOf(JavaLog{})) {
 		logEntry := item.(JavaLog)
 		// 输出前三条错误消息
 		if count < 3 {
 			fmt.Printf("第 %d 条错误消息：%s\n", count+1, logEntry.Message)
+			errorMessages = append(errorMessages, logEntry.Message)
 			count++
 		}
 	}
-
-	if count == 0 {
-		fmt.Println("没有找到错误消息")
-	}
-
 	// 构建 Kibana 查询面板链接
 	kibanaLink := fmt.Sprintf("%s/app/discover#/?_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:'%s',to:'%s'))&_a=(columns:!('@timestamp',message),filters:!(),index:%s,interval:auto,query:(language:kuery,query:''),sort:!(!('@timestamp',desc)))",
 		kibana_url, before.UTC().Format("2006-01-02T15:04:05.000Z"), now.UTC().Format("2006-01-02T15:04:05.000Z"), indexName)
 	fmt.Println("Kibana 查询面板链接:", kibanaLink)
 
+	if count == 0 {
+		fmt.Println("没有找到错误消息")
+		message := fmt.Sprintf("# Log Alert\n## <font color=\"#ff0000\">【%s】返回error异常</font>\n 错误次数：%d \n 错误日志：无\n - [点击查询告警时段日志](%s)\n",
+			indexName, count, kibanaLink)
+		log.Println(message)
+		sendWeCom(message)
+	} else {
+		// 如果有错误消息，构建完整的 message
+		errorLogList := ""
+		for i, msg := range errorMessages {
+			errorLogList += fmt.Sprintf("- 第 %d 条：%s\n", i+1, msg)
+		}
+		message := fmt.Sprintf("# Log Alert\n## <font color=\"#ff0000\">【%s】返回error异常</font>\n 错误次数：%d \n 错误日志：\n%s \n- [点击查询告警时段日志](%s)\n",
+			indexName, count, errorLogList, kibanaLink)
+		log.Println(message)
+		sendWeCom(message)
+	}
+
+}
+func sendWeCom(message string) {
+	webhook := fmt.Sprintf("https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=%s", wecom_token)
+	params := map[string]interface{}{
+		"msgtype": "markdown",
+		"markdown": map[string]interface{}{
+			"content": message,
+		},
+	}
+
+	data, _ := json.Marshal(params)
+	resp, err := http.Post(webhook, "application/json", bytes.NewBuffer(data))
+	if err != nil {
+		log.Printf("Failed to send WeCom message: %v", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	var result map[string]interface{}
+	json.NewDecoder(resp.Body).Decode(&result)
+	log.Printf("WeCom response: %v", result)
 }
